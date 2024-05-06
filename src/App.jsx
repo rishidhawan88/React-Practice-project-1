6d48074a2658922626dbf7639a72e97bf08920a5
Rishi Dhawan <rishi.dhawan@publicissapient.com>
2024-05-06 22:11:43 +0530

    implemented dynamic rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+const arr=['one','two','three'];
+
+function selectWord(maxCount){
+  return(Math.floor(Math.random()*(maxCount+1)));
+}
+
+const word=arr[selectWord(2)];
+
 function Header(){
+  
+
   return(
-    <header>
+   <header>
+    
     <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {word} React concepts you will need for almost any app you are
       going to build!
     </p>
     </header>
